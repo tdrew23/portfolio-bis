65767ee4f99071e102941402528db9b92f59a6e7
Tariq <tariqdrewniak16@gmail.com>
2025-01-03 01:55:29 +0100

    bis

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Remplacez <nom-du-repo> par le nom de votre dépôt GitHub
+  base: '/portfolio-bis/', // Remplacez <nom-du-repo> par le nom de votre dépôt GitHub
 
 })
